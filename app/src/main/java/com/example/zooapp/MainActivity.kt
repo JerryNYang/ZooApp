31c0f614832ae46e399d99480144a7a42bf9b448
jerryNYang <jerrydinyang@gmail.com>
2023-09-03 16:53:45 -0500

    Figure out LayoutInflator and kotlin synthethic plug

--- app/src/main/java/com/example/zooapp/MainActivity.kt
+++ app/src/main/java/com/example/zooapp/MainActivity.kt
@@ -2,10 +2,90 @@ package com.example.zooapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
 
 class MainActivity : AppCompatActivity() {
+
+    var listOfAnimals = ArrayList<Animal>()
+    var adapter:AnimalAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // load animals
+        listOfAnimals.add(
+            Animal("Baboo", "Baboon lives in big place...", R.drawable.baboon)
+        )
+
+        listOfAnimals.add(
+            Animal("Baboo", "Baboon lives in big place...", R.drawable.baboon)
+        )
+
+        listOfAnimals.add(
+            Animal("Bulldog", "Bulldog lives in big place...", R.drawable.bulldog)
+        )
+
+
+        listOfAnimals.add(
+            Animal("Panda", "Panda lives in big place...", R.drawable.panda)
+        )
+
+
+        listOfAnimals.add(
+            Animal("Swallow", "Swallow Bird lives in big place...", R.drawable.swallow_bird)
+        )
+
+        listOfAnimals.add(
+            Animal("Tiger", "Tiger lives in big place...", R.drawable.white_tiger)
+        )
+
+
+        listOfAnimals.add(
+            Animal("Zebra", "Zebra lives in big place...", R.drawable.zebra)
+        )
+
+        adapter = AnimalAdapter(listOfAnimals)
+
     }
+        class AnimalAdapter:BaseAdapter{
+
+           var listOfAnimals =  ArrayList<Animal>()
+            constructor( listOfAnimals: ArrayList<Animal>):super()
+            {
+
+                this.listOfAnimals = listOfAnimals
+            }
+            override fun getCount(): Int {
+
+                return listOfAnimals.size
+            }
+
+            override fun getItem(position: Int): Any {
+               return listOfAnimals[position]
+            }
+
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
+
+            }
+
+
+
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val animal = listOfAnimals[position]
+                /*
+                var myView = LayoutInflator.infalte(R.layout.animal_ticket, null)
+                myView.tvName.text = animal.name
+                myView.tvDes.text = animal.des
+                myView.tvAnimalImage.image.setImageResource(animal.image)
+                */
+                return myView
+            }
+
+        }
+
 }
